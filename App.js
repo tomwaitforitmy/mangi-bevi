03d57932dc07c8996165ff9a66fce1d28ccae878
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-10-10 13:18:13 +0200

    Add MyNavigationContainer

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 // In App.js in a new project
 
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-
-const Stack = createNativeStackNavigator();
+import MyNavigationContainer from "./navigation/MyNavigationContainer";
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <MyNavigationContainer></MyNavigationContainer>;
 }
 
 export default App;
---
+++ navigation/MyNavigationContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const MyStack = createNativeStackNavigator();
+
+const MyNavigationContainer = (props) => {
+  return (
+    <NavigationContainer>
+      <MyStack.Navigator>
+        <MyStack.Screen name="Home" component={HomeScreen} />
+      </MyStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MyNavigationContainer;
