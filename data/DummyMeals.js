873ee0c49c699be93712794e50abf9bbd576490d
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-10-10 14:12:55 +0200

    Add meal and dummy data

---
+++ data/DummyMeals.js
@@ -0,0 +1,35 @@
+import Meal from "../models/meal";
+
+export const MEALS = [
+  new Meal(
+    "m1",
+    "Spaghetti with Tomato Sauce",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg",
+    [
+      "4 Tomatoes",
+      "1 Tablespoon of Olive Oil",
+      "1 Onion",
+      "250g Spaghetti",
+      "Spices",
+      "Cheese (optional)",
+    ],
+    ["Cook Spaghetti", "Cut onion"]
+  ),
+  new Meal(
+    "m2",
+    "Toast Hawaii",
+    "https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg",
+    [
+      "1 Slice White Bread",
+      "1 Slice Ham",
+      "1 Slice Pineapple",
+      "1-2 Slices of Cheese",
+      "Butter",
+    ],
+    [
+      "Butter one side of the white bread",
+      "Layer ham, the pineapple and cheese on the white bread",
+      "Bake the toast for round about 10 minutes in the oven at 200°C",
+    ]
+  ),
+];
---
+++ models/meal.js
@@ -0,0 +1,11 @@
+class Meal {
+  constructor(title, id, imageUrl, ingredients, steps) {
+    this.title = title;
+    this.id = id;
+    this.imageUrl = imageUrl;
+    this.ingredients = ingredients;
+    this.steps = steps;
+  }
+}
+
+export default Meal;
