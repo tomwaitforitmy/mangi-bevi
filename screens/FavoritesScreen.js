d479e866ce68d25c0a5fed7e2a01c0c1286d2828
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-11-08 22:35:31 +0100

    Spike image upload in New Mangi (sometimes not working, async await screws me up :( )

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,69 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import { storage } from "../firebase";
-import uuid from "uuid";
-import LoadingIndicator from "../components/LoadingIndicator";
-import getPictureBlob from "../firebase/getPictureBlob";
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
 function FavoritesScreen({ navigation }) {
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    getPermission();
-  }, []);
-
-  const getPermission = async () => {
-    if (Platform.OS !== "web") {
-      const { status } =
-        await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== "granted") {
-        alert("Sorry, we need camera roll permissions to make this work!");
-      }
-    }
-  };
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      uploadImageToBucket(result.uri);
-    }
-  };
-
-  const uploadImageToBucket = async (uri) => {
-    let blob;
-    try {
-      setIsLoading(true);
-      blob = await getPictureBlob(uri);
-
-      const ref = await storage.ref().child(uuid.v4());
-      const snapshot = await ref.put(blob);
-
-      return await snapshot.ref.getDownloadURL();
-    } catch (e) {
-      alert(e.message);
-    } finally {
-      blob.close();
-      setIsLoading(false);
-    }
-  };
-
-  if (isLoading) {
-    return <LoadingIndicator />;
-  }
-
   return (
     <View style={styles.screen}>
       <Text> Favorites not implemented, yet. Push Tommy :)</Text>
-      <Button title="Pick Image" onPress={pickImage}></Button>
     </View>
   );
 }
--- screens/NewScreen.js
+++ screens/NewScreen.js
@@ -1,11 +1,22 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { View, StyleSheet, Text, ScrollView, Button } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Button,
+  Image,
+} from "react-native";
 import LoadingIndicator from "../components/LoadingIndicator";
 import * as mealActions from "../store/actions/mealsAction";
 import Meal from "../models/Meal";
 import { Input } from "react-native-elements";
 import MyListItem from "../components/MyListItem";
+import * as ImagePicker from "expo-image-picker";
+import { storage } from "../firebase";
+import uuid from "uuid";
+import getPictureBlob from "../firebase/getPictureBlob";
 
 function NewScreen({ navigation }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -13,35 +24,93 @@ function NewScreen({ navigation }) {
   const [ingredient, setIngredient] = useState();
   const [ingredients, setIngredients] = useState([]);
   const [step, setStep] = useState();
+  const [imageUri, setImageUri] = useState();
   const [steps, setSteps] = useState([]);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    getPermission();
+  }, []);
+
+  const getPermission = async () => {
+    if (Platform.OS !== "web") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      uploadImageToBucket(result.uri).then((uri) => setImageUri(uri));
+    }
+  };
+
+  const uploadImageToBucket = async (uri) => {
+    let blob;
+    try {
+      setIsLoading(true);
+      blob = await getPictureBlob(uri);
+
+      const ref = await storage.ref().child(uuid.v4());
+      const snapshot = await ref.put(blob);
+
+      return await snapshot.ref.getDownloadURL();
+    } catch (e) {
+      alert(e.message);
+    } finally {
+      blob.close();
+      setIsLoading(false);
+    }
+  };
+
+  const getMeal = (uri) => {
+    if (uri) {
+      return new Meal(title, "error", uri, ingredients, steps);
+    } else {
+      return new Meal(
+        title,
+        "error",
+        "https://dummyimage.com/300x200&text=No+image+reinhold+messner",
+        ingredients,
+        steps
+      );
+    }
+  };
 
   const createMealHandler = useCallback(async () => {
     if (!title || ingredients.length < 1 || steps.length < 1) {
       return;
     }
-    const newMeal = new Meal(
-      title,
-      "error",
-      "https://dummyimage.com/300x200&text=No+image+yet!",
-      ingredients,
-      steps
-    );
+
+    const newMeal = getMeal(imageUri);
 
     console.log(newMeal);
 
     try {
       setIsLoading(true);
+
       await dispatch(mealActions.createMeal(newMeal));
     } catch (err) {
       throw err;
+    } finally {
+      setIsLoading(false);
+      setSteps([]);
+      setStep();
+      setIngredients([]);
+      setIngredient();
+      setTitle();
+      setImageUri();
     }
-    setIsLoading(false);
-    setSteps([]);
-    setStep();
-    setIngredients([]);
-    setIngredient();
-    setTitle();
   }, [dispatch, title, ingredients, steps]);
 
   if (isLoading) {
@@ -116,6 +185,8 @@ function NewScreen({ navigation }) {
             }}
           ></Input>
         </View>
+        <Button title="Select image" onPress={pickImage}></Button>
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
         <Button title="Create" onPress={createMealHandler}></Button>
       </ScrollView>
     </View>
@@ -126,6 +197,10 @@ const styles = StyleSheet.create({
   list: {
     flex: 1,
     width: "100%",
+  },
+  image: {
+    width: "100%",
+    height: 200,
   },
   title: {
     fontSize: 22,
