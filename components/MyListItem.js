4daadd3dd00819386ab051667c3e94409b023547
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-11-13 18:41:11 +0100

    Add hyperlinks to MyListItem

--- components/MyListItem.js
+++ components/MyListItem.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { ListItem, Icon } from "react-native-elements";
+import HyperLink from "react-native-hyperlink";
 
 const MyListItem = (props) => {
   return (
     <View style={styles.listItemView}>
       <ListItem bottomDivider>
         <ListItem.Content>
-          <ListItem.Title>{props.title}</ListItem.Title>
+          <HyperLink linkDefault={true} linkStyle={{ color: "#2980b9" }}>
+            <ListItem.Title>{props.title}</ListItem.Title>
+          </HyperLink>
         </ListItem.Content>
         {props.IconName && (
           <Icon name={props.IconName} onPress={props.onPressIcon} />
