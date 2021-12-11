c0cdd457117766117fbf2194945f1de6f691bd0d
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-12-11 17:39:08 +0100

    Add default url earlier for empty imageUri

--- firebase/uploadImageToBucket.js
+++ firebase/uploadImageToBucket.js
@@ -3,6 +3,9 @@ import uuid from "uuid";
 import getPictureBlob from "../firebase/getPictureBlob";
 
 const uploadImageToBucket = async (uri) => {
+  if (!uri) {
+    return "https://dummyimage.com/300x200&text=No+image+reinhold+messner";
+  }
   let blob;
   try {
     blob = await getPictureBlob(uri);
