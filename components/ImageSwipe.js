46b6d0815a9e6b529da7e07965a66507adda9aa3
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-12-12 17:53:46 +0100

    Add image swipe

---
+++ components/ImageSwipe.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { Image } from "react-native-elements";
+
+const ImageSwipe = (props) => {
+  return (
+    <View style={{ ...styles.container, ...props.style }}>
+      <ScrollView
+        style={{ flex: 1 }}
+        horizontal={true}
+        decelerationRate="fast"
+        snapToInterval={props.width} //your element width
+        snapToAlignment={"center"}
+        pinchGestureEnabled={true}
+      >
+        {props.images.map((item, index) => (
+          <View style={{ width: props.width }} key={index}>
+            <Image source={{ uri: item.url }} style={styles.image}></Image>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImageSwipe;
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import { Image } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ImageSwipe from "../components/ImageSwipe";
 
 function FiltersScreen({ navigation }) {
+  // const [activeIndex, setActiveIndex] = useState(0);
+
+  const { width } = Dimensions.get("window");
+
+  const images = [
+    {
+      url: "https://images.pexels.com/photos/1382734/pexels-photo-1382734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    },
+    {
+      url: "https://images.pexels.com/photos/9413/animal-cute-kitten-cat.jpg?cs=srgb&dl=adorable-animal-cat-9413.jpg&fm=jpg",
+    },
+    {
+      url: "https://i.pinimg.com/236x/c6/6b/11/c66b111bf4df809e87a1208f75d2788b.jpg",
+    },
+    {
+      url: "https://images.pexels.com/photos/1382734/pexels-photo-1382734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    },
+    {
+      url: "https://images.pexels.com/photos/9413/animal-cute-kitten-cat.jpg?cs=srgb&dl=adorable-animal-cat-9413.jpg&fm=jpg",
+    },
+    {
+      url: "https://i.pinimg.com/236x/c6/6b/11/c66b111bf4df809e87a1208f75d2788b.jpg",
+    },
+  ];
+
+  // renderItem = ({ item, index }) => {
+  //   return (
+  //     <View style={styles.slide}>
+  //       <Image source={{ uri: item.url }} style={styles.image}></Image>
+  //       {/* <Text>{item.url}</Text> */}
+  //     </View>
+  //   );
+  // };
+
   return (
-    <View style={styles.screen}>
-      <Text> Filters not implemented, yet. Push Tommy :)</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, paddingTop: 50 }}>
+      <ImageSwipe images={images} width={width} />
+    </SafeAreaView>
   );
 }
 
@@ -14,6 +51,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
