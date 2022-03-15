64292e5e2f6dc82bf48e69c39e4cd80a421086ed
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-03-15 19:28:30 +0100

    Changes suggested by David Scholz

--- components/MyKeyboardAvoidingView.js
+++ components/MyKeyboardAvoidingView.js
@@ -3,31 +3,35 @@ import React from "react";
 import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
 
 const MyKeyboardAvoidingView = ({ children }) => {
-  if (Platform.OS == "ios") {
-    //as suggested in many places online adjust the vertical offset with the header height.
-    const headerHeight = useHeaderHeight();
+  // if (Platform.OS == "ios") {
+  //as suggested in many places online adjust the vertical offset with the header height.
+  const headerHeight = useHeaderHeight();
+  const behavior = Platform.OS === "ios" ? "padding" : "height";
+  console.log("use height: " + headerHeight);
+  console.log("behavior: " + behavior);
 
-    return (
+  return (
+    <View style={{ flex: 1 }}>
       <KeyboardAvoidingView
         style={styles.container}
-        behavior={"padding"}
-        //as suggested in many places online the magic 64 seems "ok"
-        keyboardVerticalOffset={headerHeight + 64}
+        behavior={behavior}
+        keyboardVerticalOffset={headerHeight}
       >
         {children}
       </KeyboardAvoidingView>
-    );
-  }
+    </View>
+  );
+  // }
 
   //everything works fine in Android. Do not use the KeyboardAvoidingView
-  return <View style={styles.container}>{children}</View>;
+  // return <View style={styles.container}>{children}</View>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 5,
-    width: "100%",
+    padding: 10,
+    // width: "100%",
   },
 });
 
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,11 +1,74 @@
-import React from "react";
-import { View, StyleSheet, TextInput, Button, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  Text,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import { Input } from "react-native-elements";
 import MyKeyboardAvoidingView from "../components/MyKeyboardAvoidingView";
 
 function FavoritesScreen({ navigation }) {
+  const [data, setData] = useState([
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+    {
+      id: 3,
+    },
+    {
+      id: 4,
+    },
+    {
+      id: 5,
+    },
+    {
+      id: 6,
+    },
+    {
+      id: 7,
+    },
+    {
+      id: 8,
+    },
+    {
+      id: 9,
+    },
+    {
+      id: 10,
+    },
+    {
+      id: 11,
+    },
+    {
+      id: 12,
+    },
+    {
+      id: 13,
+    },
+    {
+      id: 14,
+    },
+    {
+      id: 15,
+    },
+  ]);
+
   return (
     <MyKeyboardAvoidingView>
+      {/* <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text style={{ flex: 1, paddingVertical: 20 }}>{item.id}</Text>
+        )}
+      /> */}
       <ScrollView style={styles.scrollViewContainer}>
         <Button title="blocker"></Button>
         <Button title="blocker"></Button>
@@ -29,9 +92,21 @@ function FavoritesScreen({ navigation }) {
         <Button title="blocker"></Button>
         <Button title="blocker"></Button>
       </ScrollView>
-      <View style={styles.container}>
-        <Input placeholder="enter text"></Input>
-        <TextInput placeholder="enter text"></TextInput>
+      {/* <View style={styles.container}> */}
+      {/* <Input renderErrorMessage={true} placeholder="enter text"></Input> */}
+      {/* <TextInput placeholder="enter text"></TextInput> */}
+      <View style={{ flex: 1, marginBottom: 40 }}>
+        <TextInput
+          style={{
+            backgroundColor: "#2E2E2E",
+            width: "100%",
+            borderRadius: 18,
+            height: 36,
+            paddingLeft: 10,
+            paddingRight: 10,
+            color: "#FFFFFF",
+          }}
+        />
       </View>
     </MyKeyboardAvoidingView>
   );
@@ -42,7 +117,9 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   container: {
-    padding: 10,
+    flex: 1,
+    // padding: 10,
+    // minHeight: 50,
   },
 });
 
