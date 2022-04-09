3214415a6beebe646da5919d89bdf4ee5c886c64
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-04-09 16:33:08 +0200

    Remove login

--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -3,14 +3,12 @@ import MealList from "../components/MealList";
 import { View, StyleSheet, RefreshControl } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import * as mealsActions from "../store/actions/mealsAction";
-import * as authActions from "../store/actions/authAction";
 import * as tagActions from "../store/actions/tagsAction";
 
 function MealsScreen({ navigation }) {
   const dispatch = useDispatch();
 
   const [isLoading, setIsLoading] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
 
   const fetchAll = async () => {
@@ -25,27 +23,9 @@ function MealsScreen({ navigation }) {
   }, []);
 
   useEffect(() => {
-    if (!isLoggedIn) {
-      console.log("not logged in!");
-      authHandler().then(setIsLoggedIn(true));
-    } else {
-      console.log("logged in as tommy");
-    }
     setIsLoading(true);
     fetchAll().then(setIsLoading(false));
   }, [dispatch]);
-
-  const authHandler = async () => {
-    let action = authActions.login("tommy@test.com", "123456");
-
-    setIsLoading(true);
-    try {
-      await dispatch(action);
-    } catch (err) {
-      console.log(err);
-      setIsLoading(false);
-    }
-  };
 
   if (isLoading) {
     return <LoadingIndicator />;
