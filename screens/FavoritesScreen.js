add4c73f22aa54d0e2a43039b0470cd3c1221b4d
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-04-16 17:52:20 +0200

    first animated expirement

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,40 +1,11 @@
 import React from "react";
 import { View, StyleSheet, TextInput, Button, ScrollView } from "react-native";
 import { Input } from "react-native-elements";
+import AnimatedTest from "../components/AnimatedTest";
 import MyKeyboardAvoidingView from "../components/MyKeyboardAvoidingView";
 
 function FavoritesScreen({ navigation }) {
-  return (
-    <MyKeyboardAvoidingView>
-      <ScrollView style={styles.scrollViewContainer}>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-        <Button title="blocker"></Button>
-      </ScrollView>
-      <View style={styles.container}>
-        <Input placeholder="enter text"></Input>
-        <TextInput placeholder="enter text"></TextInput>
-      </View>
-    </MyKeyboardAvoidingView>
-  );
+  return <AnimatedTest></AnimatedTest>;
 }
 
 const styles = StyleSheet.create({
