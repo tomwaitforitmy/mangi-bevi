25bb88b43ecaf2ffc9eece63ad727b7e3f615871
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-04-26 09:16:44 +0200

    Comment out everything except of pinchToZoom

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
+import "react-native-gesture-handler";
 import * as React from "react";
-import MyNavigationContainer from "./navigation/MyNavigationContainer";
-import ReduxThunk from "redux-thunk";
+// import MyNavigationContainer from "./navigation/MyNavigationContainer";
+// import ReduxThunk from "redux-thunk";
 
 // Redux Store
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import mealsReducer from "./store/reducers/mealsReducer";
-import authReducer from "./store/reducers/authReducer";
-import tagsReducer from "./store/reducers/tagsReducer";
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// import { Provider } from "react-redux";
+// import mealsReducer from "./store/reducers/mealsReducer";
+// import authReducer from "./store/reducers/authReducer";
+// import tagsReducer from "./store/reducers/tagsReducer";
+import { PinchToZoom } from "./components/PinchToZoom";
 
-const rootReducer = combineReducers({
-  meals: mealsReducer,
-  auth: authReducer,
-  tags: tagsReducer,
-});
+// const rootReducer = combineReducers({
+//   meals: mealsReducer,
+//   auth: authReducer,
+//   tags: tagsReducer,
+// });
 
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+// const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 function App() {
   return (
-    <Provider store={store}>
-      <MyNavigationContainer />
-    </Provider>
+    <PinchToZoom></PinchToZoom>
+    // <Provider store={store}>
+    //   <MyNavigationContainer />
+    // </Provider>
   );
 }
 
