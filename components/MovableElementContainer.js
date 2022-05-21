5d5ca38cf66c40cdf70ce32ec841026c8c8333a4
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-05-21 15:10:19 +0200

    Try scrollEventThrottle to fix animations - did not change anything visibly

--- components/MovableElementContainer.js
+++ components/MovableElementContainer.js
@@ -25,6 +25,7 @@ export default function MovableElementContainer() {
           height: totalSize,
           width: "100%",
         }}
+        scrollEventThrottle={16}
       >
         {positions.value.map((e, i) => (
           <MovableElement key={i} index={i} positions={positions} />
