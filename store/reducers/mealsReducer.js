a21343bc7fb49f396fe8f9d7be3ad68f7b1ea9df
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-07-17 13:57:57 +0200

    Fix typo, remove unused code

--- store/reducers/mealsReducer.js
+++ store/reducers/mealsReducer.js
@@ -27,31 +27,16 @@ const mealsReducer = (state = initialState, action) => {
         (meal) => meal.id === action.mealId
       );
       if (existingIndex >= 0) {
-        const udpatedFavMeals = [...state.favoriteMeals];
-        udpatedFavMeals.splice(existingIndex, 1);
-        return { ...state, favoriteMeals: udpatedFavMeals };
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return { ...state, favoriteMeals: updatedFavMeals };
       } else {
         const meal = state.meals.find((meal) => meal.id === action.mealId);
         return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
       }
     }
     case SET_FILTERS: {
-      const appliedFilters = action.filters;
       const filteredMeals = state.meals;
-      //   if (appliedFilters.glutenFree && !meal.isGlutenFree) {
-      //     return false;
-      //   }
-      //   if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
-      //     return false;
-      //   }
-      //   if (appliedFilters.vegan && !meal.isVegan) {
-      //     return false;
-      //   }
-      //   if (appliedFilters.vegetarian && !meal.isVegetarian) {
-      //     return false;
-      //   }
-      //   return true;
-      // });
 
       return { ...state, filteredMeals: filteredMeals };
     }
