00148dbc45c2e6fa67b56e2d55095386581be836
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-08-20 12:20:38 +0200

    Stopping gesture if it is too fast

--- components/MovableElement.js
+++ components/MovableElement.js
@@ -29,9 +29,9 @@ function MovableElement({ index, positions }) {
     () => positions.value[index].position, //listen to value[id] for changes made by gesture
     (currentPosition, previousPosition) => {
       "worklet";
-      if (currentPosition !== previousPosition) {
-        //only if there is change in position
-        if (!isGestureActive.value) {
+      if (!isGestureActive.value) {
+        if (currentPosition !== previousPosition) {
+          //only if there is change in position
           //only if this element is not getting touched
           //update the translation with a nice animation
           translateY.value = withSpring(currentPosition, animationConfig);
@@ -59,43 +59,54 @@ function MovableElement({ index, positions }) {
   });
 
   const pan = Gesture.Pan().onChange(
-    ({ translationX, translationY, absoluteY }) => {
+    ({ translationX, translationY, absoluteY, velocityY }) => {
       "worklet";
-      //translate by the correct value:
-      //translationY is relative, accumulated over the whole gesture
-      //offsetY.value is the starting point, determined by the size of the whole thing
-      translateY.value = offsetY.value + translationY;
+      if (Math.abs(velocityY) > 1125) {
+        console.log("too fast!");
+        isGestureActive.value = false;
+        return;
+      }
 
-      //get the position Id based on movement
-      const newPositionId = sortedIndex(
-        positions.value.map((e) => e.threshold),
-        translateY.value
-      );
-
-      // //remove values out of range
-      // const newOrderCandidate = clamp(
-      //   newPositionId,
-      //   0,
-      //   Object.keys(positions.value).length - 1
-      // );
-
-      // Swap the positions if needed
-      if (newOrderCandidate > positions.value[index].order) {
-        const to = positions.value.findIndex(
-          (e) => e.order === newOrderCandidate
+      if (isGestureActive.value) {
+        //translate by the correct value:
+        //translationY is relative, accumulated over the whole gesture
+        //offsetY.value is the starting point, determined by the size of the whole thing
+        translateY.value = offsetY.value + translationY;
+        //get the position Id based on movement
+        const newPositionId = sortedIndex(
+          positions.value.map((e) => e.threshold),
+          translateY.value
         );
 
-        positions.value = swapElementWithHigherOrder(
-          positions.value,
-          index,
-          to
-        );
-      } else if (newOrderCandidate < positions.value[index].order) {
-        const to = positions.value.findIndex(
-          (e) => e.order === newOrderCandidate
-        );
+        // //remove values out of range
+        // const newOrderCandidate = clamp(
+        //   newPositionId,
+        //   0,
+        //   Object.keys(positions.value).length - 1
+        // );
 
-        positions.value = swapElementWithLowerOrder(positions.value, index, to);
+        // Swap the positions if needed
+        if (newPositionId > positions.value[index].order) {
+          const to = positions.value.findIndex(
+            (e) => e.order === newPositionId
+          );
+
+          positions.value = swapElementWithHigherOrder(
+            positions.value,
+            index,
+            to
+          );
+        } else if (newPositionId < positions.value[index].order) {
+          const to = positions.value.findIndex(
+            (e) => e.order === newPositionId
+          );
+
+          positions.value = swapElementWithLowerOrder(
+            positions.value,
+            index,
+            to
+          );
+        }
       }
     }
   );
