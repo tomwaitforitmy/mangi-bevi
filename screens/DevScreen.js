90754bf7af410be0d2ed2632a8dfa19ea8558d5e
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-08-20 16:34:35 +0200

    Nice! Finally a drag-sort that is working

--- screens/DevScreen.js
+++ screens/DevScreen.js
@@ -1,12 +1,42 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
 import MovableElementContainer from "../components/MovableElementContainer";
 import BulkEditMeal from "../components/BulkEditMeal";
+import { ELEMENTS } from "../components/MovableElementContainerConfig";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import DraggableFlatList, {
+  ScaleDecorator,
+} from "react-native-draggable-flatlist";
+import Element from "../components/Element";
 
 function DevScreen({ navigation }) {
+  const [data, setData] = useState(ELEMENTS);
+
+  const renderItem = ({ item, drag, isActive }) => {
+    return (
+      <ScaleDecorator>
+        <TouchableOpacity
+          onLongPress={drag}
+          disabled={isActive}
+          style={[
+            styles.rowItem,
+            { backgroundColor: isActive ? "red" : "blue" },
+          ]}
+        >
+          <Element title={item.title}></Element>
+        </TouchableOpacity>
+      </ScaleDecorator>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <MovableElementContainer></MovableElementContainer>
+      {/* <MovableElementContainer></MovableElementContainer> */}
+      <DraggableFlatList
+        data={data}
+        onDragEnd={({ data }) => setData(data)}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
