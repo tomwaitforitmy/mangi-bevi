d760736d84eb7d7e028e3b173766e97b4f87f11d
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-08-27 17:19:42 +0200

    Replace dummy test with some slightly more useful

---
+++ tests/component-tests/MyButton.spec.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react-native";
+import MyButton from "../../components/MyButton";
+
+describe("MyButton", () => {
+  it("renders the correct message", () => {
+    render(<MyButton>{"Create Tag"}</MyButton>);
+    expect(screen.queryByText("Create Tag")).toBeTruthy();
+  });
+});
