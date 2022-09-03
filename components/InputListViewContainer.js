3c21049a153583b8b567cc7cfd01ef5f90d67b5e
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-09-03 16:12:01 +0200

    Fixed issue where empty data could throw in tests

--- components/InputListViewContainer.js
+++ components/InputListViewContainer.js
@@ -8,14 +8,15 @@ const InputListViewContainer = (props) => {
     <View style={{ ...styles.container, ...props.style }}>
       <TouchableOpacity onLongPress={props.onLongPress}>
         <Text style={styles.subtitle}>{props.title}</Text>
-        {props.data.map((e) => (
-          <MyListItem
-            key={e}
-            title={e}
-            IconName={"edit"}
-            onPressIcon={() => props.onPressIcon(e)}
-          />
-        ))}
+        {props.data &&
+          props.data.map((e) => (
+            <MyListItem
+              key={e}
+              title={e}
+              IconName={"edit"}
+              onPressIcon={() => props.onPressIcon(e)}
+            />
+          ))}
       </TouchableOpacity>
       <Input
         placeholder="Enter text"
