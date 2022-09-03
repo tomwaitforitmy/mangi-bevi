9ab6eb8bdf6e503bb62091e466bbfde0fe52a21d
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-09-03 16:13:09 +0200

    Add test for long title

--- tests/component-tests/MyListItem.spec.js
+++ tests/component-tests/MyListItem.spec.js
@@ -3,8 +3,18 @@ import { render, screen } from "@testing-library/react-native";
 import MyListItem from "../../components/MyListItem";
 
 describe("MyButton", () => {
-  it("renders the correct message", () => {
+  it("renders the correct title", () => {
     render(<MyListItem title="Tomatoes" />);
     expect(screen.queryByText("Tomatoes")).toBeTruthy();
+  });
+  it("renders long titles", () => {
+    render(
+      <MyListItem title="10000 million trillion billion tomatoes went on a long trip into a hot pan to get sweet nice tomato sauce. They stayed happily warm forever until they reached my belly." />,
+    );
+    expect(
+      screen.queryByText(
+        "10000 million trillion billion tomatoes went on a long trip into a hot pan to get sweet nice tomato sauce. They stayed happily warm forever until they reached my belly.",
+      ),
+    ).toBeTruthy();
   });
 });
