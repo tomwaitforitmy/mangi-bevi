269b7ae5d0c60cda7829c10f4187661488f8bc71
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-09-10 12:49:08 +0200

    Add experiment for layout animations. It breaks somehow the whole App layout! :(

--- screens/DevScreen.js
+++ screens/DevScreen.js
@@ -1,29 +1,11 @@
 import React, { useState } from "react";
-import MovableElementContainer from "../experiments/MoveableElementContainer-Components/MovableElementContainer";
-import BulkEditMeal from "../components/BulkEditMeal";
-import { ELEMENTS } from "../experiments/MoveableElementContainer-Components/MovableElementContainerConfig";
 import { Text, View, StyleSheet } from "react-native";
-import DraggableItemList from "../components/DraggableItemList";
-import { useSelector } from "react-redux";
-import { ConvertArrayToArrayOfObjects } from "../common_functions/ConvertArrayToArrayOfObjects";
+import LayoutAnimations from "../experiments/LayoutAnimations";
 
 function DevScreen({ navigation }) {
-  const id = "-N93DNXIQFETNW8zh5iV";
-  const inputMeal = useSelector((state) =>
-    state.meals.meals.find((m) => m.id === id),
-  );
-
-  const myArray = ["Cheese", "Tomatoes", "Water"];
-  const initialData = ConvertArrayToArrayOfObjects(inputMeal.ingredients);
-
-  const [inputData, setInputData] = useState(inputMeal.ingredients);
-  const onSortEnd = (sortedData) => {
-    setInputData(sortedData);
-  };
-
   return (
     <View style={styles.container}>
-      <DraggableItemList data={inputData} onSortEnd={onSortEnd} />
+      <LayoutAnimations />
     </View>
   );
 }
