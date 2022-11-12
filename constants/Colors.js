f3efd6f7f4daf138810979048feb15f0f7155412
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-11-12 17:07:18 +0100

    Add colors of level view

--- constants/Colors.js
+++ constants/Colors.js
@@ -6,4 +6,9 @@ export default {
   speedDialIcon: "white",
   screenBackGround: "#f2f2f2",
   hyperlink: "#2980b9",
+  levelViewBackground: "white",
+  levelViewBarBackground: "lightgreen",
+  levelViewBar: "green",
+  levelViewBarBackgroundBorder: "darkgreen",
+  levelViewTexts: "black",
 };
