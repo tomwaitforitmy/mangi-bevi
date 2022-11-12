f42b7cdd64dac951ffaadc7b9718362d75ac8a39
Thomas Kilgus <tomwaitforitmy@gmail.com>
2022-11-12 15:09:33 +0100

    Work on levelView

--- screens/DevScreen.js
+++ screens/DevScreen.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
-import LayoutAnimations from "../experiments/LayoutAnimations";
+import { Text, View, StyleSheet, Button } from "react-native";
+import LevelView from "../components/LevelView";
+import { RECIPE_REWARDS } from "../data/RecipeRewards";
+import { TAG_REWARDS } from "../data/TagRewards";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function DevScreen({ navigation }) {
+  const [firstValue, setFirstValue] = useState(32);
+
   return (
     <View style={styles.container}>
-      <LayoutAnimations />
+      <LevelView value={firstValue} rewards={RECIPE_REWARDS} />
+      <LevelView value={12} rewards={TAG_REWARDS} />
+      <LevelView value={0} rewards={RECIPE_REWARDS} />
+      <Button
+        title="random"
+        onPress={() => {
+          setFirstValue(getRandomInt(100));
+        }}
+      />
     </View>
   );
 }
