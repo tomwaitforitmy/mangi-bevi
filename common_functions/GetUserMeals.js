69aac7166d6af0403409c473e574d1a4c2e699bb
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-01-14 15:08:42 +0100

    Print out inconsistent data

--- common_functions/GetUserMeals.js
+++ common_functions/GetUserMeals.js
@@ -4,6 +4,8 @@ export const GetUserMeals = (meals, userMeals) => {
     const found = meals.find((m) => m.id === id);
     if (found) {
       result.push(found);
+    } else {
+      console.log("Error! Could not find meal id '" + id + "' in meals");
     }
   });
 
