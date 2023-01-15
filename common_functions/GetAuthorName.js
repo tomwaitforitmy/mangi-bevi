cc2d23808c688d526512c2fe24d992714f10670e
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-01-15 15:39:02 +0100

    Add GetAuthor

--- common_functions/GetAuthorName.js
+++ common_functions/GetAuthorName.js
@@ -1,9 +1,19 @@
 export const GetAuthorNameByMealId = (mealId, users) => {
-  const author = users.find((user) => user.meals.includes(mealId));
+  const author = GetAuthorByMealId(mealId, users);
   return author.name;
 };
 
 export const GetAuthorName = (userId, users) => {
-  const author = users.find((user) => user.id === userId);
+  const author = GetAuthor(userId, users);
   return author.name;
+};
+
+export const GetAuthor = (userId, users) => {
+  const author = users.find((user) => user.id === userId);
+  return author;
+};
+
+export const GetAuthorByMealId = (mealId, users) => {
+  const author = users.find((user) => user.meals.includes(mealId));
+  return author;
 };
