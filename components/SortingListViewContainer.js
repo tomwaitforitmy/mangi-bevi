fbd4eda56448680621ff548ecd278544f47dbd6c
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-10 14:06:14 +0200

    Move done button down, because it is easier to hit

--- components/SortingListViewContainer.js
+++ components/SortingListViewContainer.js
@@ -6,11 +6,11 @@ import MyButton from "./MyButton";
 const SortingListViewContainer = (props) => {
   return (
     <View style={{ ...styles.container, ...props.style }}>
-      <View style={styles.button}>
-        <MyButton onPress={props.onPressDoneSorting}>{"Done sorting"}</MyButton>
-      </View>
       <View style={styles.draggableList}>
         <DraggableItemList data={props.data} onSortEnd={props.onSortEnd} />
+      </View>
+      <View style={styles.button}>
+        <MyButton onPress={props.onPressDoneSorting}>{"Done sorting"}</MyButton>
       </View>
     </View>
   );
