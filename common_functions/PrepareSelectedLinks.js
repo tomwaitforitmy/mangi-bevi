1c170ce657c2cbb87d4339c49319914513dd2406
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-14 19:25:03 +0200

    Fix a bug where too many meals would be pre-selected

--- common_functions/PrepareSelectedLinks.js
+++ common_functions/PrepareSelectedLinks.js
@@ -1,4 +1,9 @@
 export const PrepareSelectedLinks = (candidates, links) => {
+  // reset everything that might have been selected for other meal
+  candidates.forEach((meal) => {
+    meal.isSelected = false;
+  });
+
   if (links) {
     links.map((id) => {
       candidates.find((m) => m.id === id).isSelected = true;
