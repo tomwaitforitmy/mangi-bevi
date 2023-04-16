575ba93132b335f91d84914f6d419ddebae5771c
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-16 16:31:20 +0200

    Add TinyMealList

---
+++ components/TinyMealList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import TinyMealItem from "./TinyMealItem";
+
+const TinyMealList = ({ meals, style, onPressMeal }) => {
+  return (
+    <View style={{ ...styles.container, ...style }}>
+      <FlatList
+        data={meals}
+        renderItem={({ item }) => (
+          <TinyMealItem meal={item} onPressMeal={onPressMeal} />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+});
+
+export default TinyMealList;
