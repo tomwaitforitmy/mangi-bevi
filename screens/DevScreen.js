d51616252531e63007c0dd40a6587863173a50ea
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-16 16:31:51 +0200

    Use Components

--- screens/DevScreen.js
+++ screens/DevScreen.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { View, StyleSheet, TextInput } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import BulkEditMeal from "../components/BulkEditMeal";
 import LevelsViewModal from "../components/LevelsViewModal";
-import MultiSelectMealsList from "../components/MultiSelectMealsList";
 import { FastFilterMeals } from "../common_functions/FastFilterMeals";
-import { Input } from "react-native-elements";
-import Colors from "../constants/Colors";
+import TinyMealList from "../components/TinyMealList";
+import SearchInput from "../components/SearchInput";
 
 function DevScreen({ navigation }) {
   const allMeals = useSelector((state) => state.meals.meals);
@@ -16,41 +15,22 @@ function DevScreen({ navigation }) {
 
   console.log(lessMeals.length);
 
-  const onEndSelection = (meals) => {
-    const selectedMeals = meals.filter((m) => m.isSelected);
-    selectedMeals.map((m) => console.log(m.title));
-  };
-
   const onChangeText = (text) => {
     setSearchTerm(text.toLowerCase());
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <TextInput
-          placeholder="Enter text"
-          onChangeText={(value) => onChangeText(value)}
-          style={styles.searchTerm}
-        />
-      </View>
-      <MultiSelectMealsList meals={lessMeals} onEndSelection={onEndSelection} />
+      <SearchInput onChangeText={onChangeText} />
+      <TinyMealList
+        meals={lessMeals}
+        onPressMeal={(m) => console.log(m.title)}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  searchTerm: {
-    backgroundColor: Colors.screenBackGround,
-    fontSize: 14,
-    margin: 5,
-    paddingLeft: 10,
-    borderRadius: 25,
-    color: "black",
-  },
-  textContainer: {
-    backgroundColor: Colors.primary,
-  },
   container: {
     flex: 1,
     width: "100%",
