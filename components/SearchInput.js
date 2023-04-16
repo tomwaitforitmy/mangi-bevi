58323c09b35b85cf4990859c7b92203fcbbf6e8e
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-16 16:31:31 +0200

    Add SearchInput

---
+++ components/SearchInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import Colors from "../constants/Colors";
+
+const SearchInput = ({ style, onChangeText }) => {
+  return (
+    <View style={{ ...styles.textContainer, ...style }}>
+      <TextInput
+        placeholder="Enter text"
+        onChangeText={(value) => onChangeText(value)}
+        style={styles.searchTerm}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchTerm: {
+    backgroundColor: Colors.screenBackGround,
+    fontSize: 14,
+    margin: 5,
+    paddingLeft: 10,
+    borderRadius: 25,
+    color: "black",
+  },
+  textContainer: {
+    backgroundColor: Colors.primary,
+    flex: 1,
+    width: "100%",
+  },
+});
+
+export default SearchInput;
