e6d6713b408bcd452b66f43e87387bebdde66b53
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-16 15:39:37 +0200

    First version of search in dev screen

--- screens/DevScreen.js
+++ screens/DevScreen.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput } from "react-native";
 import { useSelector } from "react-redux";
 import BulkEditMeal from "../components/BulkEditMeal";
 import LevelsViewModal from "../components/LevelsViewModal";
 import MultiSelectMealsList from "../components/MultiSelectMealsList";
+import { FastFilterMeals } from "../common_functions/FastFilterMeals";
+import { Input } from "react-native-elements";
+import Colors from "../constants/Colors";
 
 function DevScreen({ navigation }) {
   const allMeals = useSelector((state) => state.meals.meals);
-  const lessMeals = allMeals.slice(0, 100);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const lessMeals = FastFilterMeals(allMeals, searchTerm);
 
   console.log(lessMeals.length);
 
@@ -16,14 +21,36 @@ function DevScreen({ navigation }) {
     selectedMeals.map((m) => console.log(m.title));
   };
 
+  const onChangeText = (text) => {
+    setSearchTerm(text.toLowerCase());
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <TextInput
+          placeholder="Enter text"
+          onChangeText={(value) => onChangeText(value)}
+          style={styles.searchTerm}
+        />
+      </View>
       <MultiSelectMealsList meals={lessMeals} onEndSelection={onEndSelection} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  searchTerm: {
+    backgroundColor: Colors.screenBackGround,
+    fontSize: 14,
+    margin: 5,
+    paddingLeft: 10,
+    borderRadius: 25,
+    color: "black",
+  },
+  textContainer: {
+    backgroundColor: Colors.primary,
+  },
   container: {
     flex: 1,
     width: "100%",
