ad933dc1fa213287f23012ef1806ed7b6a5f5d41
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-17 19:13:58 +0200

    Minor changes

--- screens/DevScreen.js
+++ screens/DevScreen.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import BulkEditMeal from "../components/BulkEditMeal";
 import LevelsViewModal from "../components/LevelsViewModal";
 import { FastFilterMeals } from "../common_functions/FastFilterMeals";
 import TinyMealList from "../components/TinyMealList";
 import SearchInput from "../components/SearchInput";
+import * as searchAction from "../store/actions/searchAction";
 
 function DevScreen({ navigation }) {
   const allMeals = useSelector((state) => state.meals.meals);
   const [searchTerm, setSearchTerm] = useState("");
+  const dispatch = useDispatch();
 
   const lessMeals = FastFilterMeals(allMeals, searchTerm);
 
   console.log(lessMeals.length);
 
-  const onChangeText = (text) => {
+  const onChangeText = async (text) => {
     setSearchTerm(text.toLowerCase());
+    await dispatch(searchAction.setSearchTerm(text));
   };
 
   return (
