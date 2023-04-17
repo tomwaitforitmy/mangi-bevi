5e894de006794f77b5d69d4d50137199689f9154
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-04-17 19:17:17 +0200

    Style changes

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import Colors from "../constants/Colors";
+import { Input } from "react-native-elements";
 
 const SearchInput = ({ style, onChangeText }) => {
   return (
     <View style={{ ...styles.textContainer, ...style }}>
-      <TextInput
+      <Input
         placeholder="Enter text"
         onChangeText={(value) => onChangeText(value)}
         style={styles.searchTerm}
+        renderErrorMessage={false}
       />
     </View>
   );
@@ -17,14 +19,15 @@ const SearchInput = ({ style, onChangeText }) => {
 const styles = StyleSheet.create({
   searchTerm: {
     backgroundColor: Colors.screenBackGround,
-    fontSize: 14,
-    margin: 5,
-    paddingLeft: 10,
+    fontSize: 18,
+    margin: 3,
+    paddingLeft: 15,
     borderRadius: 25,
     color: "black",
   },
   textContainer: {
     backgroundColor: Colors.primary,
+    width: "100%",
   },
 });
 
