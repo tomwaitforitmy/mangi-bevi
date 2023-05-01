b4fb563ccad8a2cf8dcd7a062377dcd89c5adf0a
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-05-01 16:01:35 +0200

    Remove include

--- components/MyListItem.js
+++ components/MyListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { ListItem, Icon } from "react-native-elements";
 import Hyperlink from "react-native-hyperlink";
 import Colors from "../constants/Colors";
