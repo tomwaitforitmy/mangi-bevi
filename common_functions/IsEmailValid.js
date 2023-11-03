37fc27dfb21de133ee460d4bea66b496ff510f20
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-11-03 18:47:47 +0100

    No tabs/whitespace in emails

--- common_functions/IsEmailValid.js
+++ common_functions/IsEmailValid.js
@@ -1,3 +1,11 @@
 export const IsEmailValid = (email) => {
+  if (hasWhiteSpace(email)) {
+    return false;
+  }
+
   return email.includes("@") && email.includes(".");
 };
+
+function hasWhiteSpace(s) {
+  return /\s/g.test(s);
+}
