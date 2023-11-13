610a4a5665c9c796efa2ff7d71527fbdbe78abf3
Thomas Kilgus <tomwaitforitmy@gmail.com>
2023-11-13 17:52:06 +0100

    Add ScrollView

--- components/InputListViewContainer.js
+++ components/InputListViewContainer.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View, ScrollView } from "react-native";
 import { Input } from "react-native-elements";
 import MyListItem from "./MyListItem";
 
 const InputListViewContainer = (props) => {
   return (
     <View style={{ ...styles.container, ...props.style }}>
-      <TouchableOpacity onLongPress={props.onLongPress}>
-        <Text style={styles.subtitle}>{props.title}</Text>
-        {props.data &&
-          props.data.map((e) => (
-            <MyListItem
-              key={e}
-              title={e}
-              IconName={"edit"}
-              onPressIcon={() => props.onPressIcon(e)}
-            />
-          ))}
-      </TouchableOpacity>
+      <ScrollView style={styles.scrollList}>
+        <TouchableOpacity onLongPress={props.onLongPress}>
+          {props.data &&
+            props.data.map((e) => (
+              <MyListItem
+                key={e}
+                title={e}
+                IconName={"edit"}
+                onPressIcon={() => props.onPressIcon(e)}
+              />
+            ))}
+        </TouchableOpacity>
+      </ScrollView>
       <Input
-        placeholder="Enter text"
+        placeholder={"Enter " + props.title}
         ref={props.inputRef}
         onChangeText={(value) => props.onChangeText(value)}
         onBlur={props.onBlur}
@@ -30,9 +31,9 @@ const InputListViewContainer = (props) => {
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  subtitle: {
-    fontSize: 18,
-    textAlign: "left",
+  scrollList: {
+    flex: 1,
+    width: "100%",
   },
 });
 
