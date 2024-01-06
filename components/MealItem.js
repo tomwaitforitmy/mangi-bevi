376d6ba035b674da8a8f503b11583f22cc2ee9e7
Thomas Kilgus <tomwaitforitmy@gmail.com>
2024-01-06 17:50:14 +0100

    First styling for christoph

--- components/MealItem.js
+++ components/MealItem.js
@@ -5,31 +5,33 @@ import {
   View,
   TouchableOpacity,
   ImageBackground,
+  Image,
 } from "react-native";
 import HighlightedText from "./HighlightedText";
+import Colors from "../constants/Colors";
 
 const MealItem = (props) => {
   return (
     <View style={{ ...styles.mealItem, ...props.style }}>
       <TouchableOpacity onPress={props.onSelectMeal}>
-        <View>
-          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
-            <ImageBackground
+        <View style={styles.mealRow}>
+          <View style={styles.imageContainer}>
+            <Image
               source={{
                 uri: props.image
                   ? props.image
                   : "https://dummyimage.com/300x200&text=No+image+yet",
               }}
-              style={styles.backgroundImage}>
-              <View style={styles.titleContainer}>
-                <Text style={styles.title} numberOfLines={1}>
-                  <HighlightedText
-                    text={props.title}
-                    searchTerm={props.searchTerm}
-                  />
-                </Text>
-              </View>
-            </ImageBackground>
+              style={styles.backgroundImage}
+            />
+          </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title} numberOfLines={1} ellipsizeMode="clip">
+              <HighlightedText
+                text={props.title}
+                searchTerm={props.searchTerm}
+              />
+            </Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -41,30 +43,43 @@ const styles = StyleSheet.create({
   mealItem: {
     flex: 1,
     height: 200,
-    backgroundColor: "#ccc",
+    // backgroundColor: "blue",
     borderRadius: 3,
     overflow: "hidden",
-    margin: 10,
+    // margin: 10,
+    marginBottom: 8,
+    // marginLeft: 5,
+    // marginRight: 5,
   },
   mealRow: {
-    flexDirection: "row",
+    // flexDirection: "column",
+    // height: "100%", //change back to 85% tommy if you want more space below item
   },
-  mealHeader: {
-    height: "100%", //change back to 85% tommy if you want more space below item
+  imageContainer: {
+    height: "85%",
+    borderRadius: 3,
   },
   backgroundImage: {
-    width: "100%",
+    // width: "100%",
     height: "100%",
-    justifyContent: "flex-end",
+    // justifyContent: "flex-end",
   },
   title: {
-    fontSize: 20,
-    color: "white",
+    fontSize: 14,
+    // color: "white",
+    overflow: "hidden",
+    // textAlign: "left",
+    textAlignVertical: "center",
+    color: "black",
   },
   titleContainer: {
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    backgroundColor: "rgba(0,0,0,0.5)",
+    // marginTop: 2,
+    borderRadius: 3,
+    justifyContent: "center",
+    height: "15%",
+    // paddingVertical: 5,
+    paddingHorizontal: 5,
+    backgroundColor: "darkgrey",
   },
 });
 
