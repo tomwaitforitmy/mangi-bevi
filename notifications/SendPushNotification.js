4acaa8bc1d485e163ba9835bf8092e42b5cc3397
Thomas Kilgus <tomwaitforitmy@gmail.com>
2024-01-13 14:54:18 +0100

    Add more safety

--- notifications/SendPushNotification.js
+++ notifications/SendPushNotification.js
@@ -1,4 +1,11 @@
 export async function sendPushNotification(expoPushToken, title, body, data) {
+  if (!expoPushToken) {
+    return;
+  }
+  if (expoPushToken === "no token") {
+    return;
+  }
+
   const message = {
     to: expoPushToken,
     sound: "default",
@@ -7,13 +14,18 @@ export async function sendPushNotification(expoPushToken, title, body, data) {
     data: data,
   };
 
-  await fetch("https://exp.host/--/api/v2/push/send", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Accept-encoding": "gzip, deflate", //todo: hmmm?
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(message),
-  });
+  try {
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate", //todo: hmmm?
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  } catch (error) {
+    console.error("Cannot send push notification");
+    console.error(error);
+  }
 }
