b49f8da74db54a2ad86ddfa10c7db163f3d20377
Thomas Kilgus <tomwaitforitmy@gmail.com>
2024-01-28 12:10:18 +0100

    Add style

--- components/ReactionsBox.js
+++ components/ReactionsBox.js
@@ -9,9 +9,17 @@ const ReactionsBox = (props) => {
     <View style={{ ...styles.container, ...props.style }}>
       {reactionAmounts.map((r) => {
         if (r.amount === 1) {
-          return <Text key={r.emoji}>{r.emoji}</Text>;
+          return (
+            <Text style={styles.emoji} key={r.emoji}>
+              {r.emoji}
+            </Text>
+          );
         } else {
-          return <Text key={r.emoji}>{r.emoji + r.amount}</Text>;
+          return (
+            <Text style={styles.emoji} key={r.emoji}>
+              {r.emoji + r.amount}
+            </Text>
+          );
         }
       })}
     </View>
@@ -20,6 +28,16 @@ const ReactionsBox = (props) => {
 
 const styles = StyleSheet.create({
   container: { flex: 1, flexDirection: "row" },
+  emoji: {
+    borderWidth: 1,
+    borderColor: "blue",
+    borderRadius: 15,
+    height: 28,
+    padding: 5,
+    backgroundColor: "lightblue",
+    overflow: "hidden",
+    margin: 1,
+  },
 });
 
 export default ReactionsBox;
