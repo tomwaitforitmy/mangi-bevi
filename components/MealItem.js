6087deca05ec00c2a5c4d0d67c1b374827876d18
Thomas Kilgus <tomwaitforitmy@gmail.com>
2024-01-28 12:16:22 +0100

    New style for mealItem

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  ImageBackground,
-  Image,
-} from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import HighlightedText from "./HighlightedText";
 import Colors from "../constants/Colors";
 
@@ -14,26 +7,21 @@ const MealItem = (props) => {
   return (
     <View style={{ ...styles.mealItem, ...props.style }}>
       <TouchableOpacity onPress={props.onSelectMeal}>
-        <View style={styles.mealRow}>
-          <View style={styles.imageContainer}>
-            <Image
-              source={{
-                uri: props.image
-                  ? props.image
-                  : "https://dummyimage.com/300x200&text=No+image+yet",
-              }}
-              style={styles.backgroundImage}
-            />
-          </View>
-          <View style={styles.titleContainer}>
-            {/* ellipsizeMode="tail" -> "..." on android */}
-            <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
-              <HighlightedText
-                text={props.title}
-                searchTerm={props.searchTerm}
-              />
-            </Text>
-          </View>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{
+              uri: props.image
+                ? props.image
+                : "https://dummyimage.com/300x200&text=No+image+yet",
+            }}
+            style={styles.backgroundImage}
+          />
+        </View>
+        <View style={styles.titleContainer}>
+          {/* ellipsizeMode="tail" -> "..." on android */}
+          <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
+            <HighlightedText text={props.title} searchTerm={props.searchTerm} />
+          </Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -44,43 +32,29 @@ const styles = StyleSheet.create({
   mealItem: {
     flex: 1,
     height: 200,
-    // backgroundColor: "blue",
-    borderRadius: 3,
+    borderRadius: 10,
     overflow: "hidden",
-    // margin: 10,
     marginBottom: 20,
-    // marginLeft: 5,
-    // marginRight: 5,
-  },
-  mealRow: {
-    // flexDirection: "column",
-    // height: "100%", //change back to 85% tommy if you want more space below item
   },
   imageContainer: {
     height: "85%",
     borderRadius: 3,
   },
   backgroundImage: {
-    // width: "100%",
     height: "100%",
-    // justifyContent: "flex-end",
   },
   title: {
     fontSize: 20,
-    // color: "white",
     overflow: "hidden",
-    // textAlign: "left",
     textAlignVertical: "center",
     color: "black",
   },
   titleContainer: {
-    // marginTop: 2,
     borderRadius: 3,
     justifyContent: "center",
     height: "15%",
-    // paddingVertical: 5,
     paddingHorizontal: 5,
-    backgroundColor: Colors.screenBackGround,
+    backgroundColor: Colors.white,
   },
 });
 
