93eb6bd33d0bcf0693ee5ade2fb00f5c60077420
Thomas Kilgus <tomwaitforitmy@gmail.com>
2024-07-19 18:54:14 +0200

    Unify authAction with firebase SDK

--- store/actions/authAction.js
+++ store/actions/authAction.js
@@ -1,19 +1,21 @@
-import { HandleResponseError } from "../../common_functions/HandleResponseError";
 import {
   ResetStorage,
   SaveCredentialsToStorage,
 } from "../../common_functions/CredentialStorage";
-import { firebaseAuth, firebaseConfig } from "../../firebase/firebase";
+import { firebaseAuth } from "../../firebase/firebase";
 import User from "../../models/User";
-import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  deleteUser,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 //---------------------------------------------
 // Authentication is for the firebase accounts
+// official docs https://firebase.google.com/docs/auth/web/manage-users
 //---------------------------------------------
-export const AUTHENTICATE = "AUTHENTICATE";
-export const LOGOUT = "LOGOUT";
-
-const FIREBASE_API_KEY = firebaseConfig.apiKey;
 
 export const getToken = async () => {
   try {
@@ -21,136 +23,71 @@ export const getToken = async () => {
     return token;
   } catch (error) {
     console.error(
-      "Could not get firebase token! User:" + firebaseAuth.currentUser,
+      "Error getIdToken:" + error + " User:" + firebaseAuth.currentUser,
     );
     throw error;
   }
 };
 
-export const authenticate = (token, userId) => {
-  return (dispatch) => {
-    dispatch({ type: AUTHENTICATE, token: token, userId: userId });
-  };
-};
-
-export const logout = () => {
-  return async (dispatch) => {
+export const logout = async () => {
+  try {
     await signOut(firebaseAuth);
     ResetStorage();
-
-    dispatch({ type: LOGOUT });
-  };
+  } catch (error) {
+    console.error("Error logout:" + error);
+  }
 };
 
-export const signup = (email, password, username) => {
-  return async (dispatch) => {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          returnSecureToken: true,
-        }),
-      },
+export const signup = async (email, password, username) => {
+  try {
+    const response = await createUserWithEmailAndPassword(
+      firebaseAuth,
+      email,
+      password,
     );
 
-    await HandleResponseError(response);
-
-    const responseData = await response.json();
-
-    await dispatch(authenticate(responseData.idToken, responseData.localId));
-
-    SaveCredentialsToStorage(email, password);
-
-    const user = User("error", username, email, [], responseData.localId);
-
+    const user = User("error", username, email, [], response.user.uid);
     return user;
-  };
+  } catch (error) {
+    console.error("Error signup:" + error);
+  }
 };
 
-export const resetPass = (email) => {
-  //together with redux-thunk, this wrapper is needed
-  //even if we technically don't need dispatch
-  return async (dispatch) => {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${FIREBASE_API_KEY}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: email,
-          requestType: "PASSWORD_RESET",
-        }),
-      },
-    );
-
-    await HandleResponseError(response);
-
-    if (response.ok) {
-      console.log("Password reset successful for " + email);
-    }
-
-    //reset store and timer here
-    logout();
-  };
+export const resetPass = async (email) => {
+  try {
+    await sendPasswordResetEmail(firebaseAuth, email);
+    ResetStorage();
+    console.log("Password reset successful for " + email);
+  } catch (error) {
+    console.error("Error reset password:" + error);
+  }
 };
 
-export const deleteAccount = () => {
-  //together with redux-thunk, this wrapper is needed
-  //even if we technically don't need dispatch
-  return async (dispatch) => {
-    const firebaseId = firebaseAuth.currentUser.uid;
-    const token = await getToken();
+export const deleteAccount = async () => {
+  try {
+    const firebaseUser = firebaseAuth.currentUser;
+    console.log("begin delete account " + firebaseUser.email);
 
-    console.log("begin delete account " + firebaseId);
-
-    if (firebaseId === "JfrGXVhcsNY78LnqQSie0GTcj692") {
+    if (firebaseUser.uid === "JfrGXVhcsNY78LnqQSie0GTcj692") {
       console.error("Stop deleting your account tommy!");
     } else {
-      const response = await fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:delete?key=${FIREBASE_API_KEY}`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            idToken: token,
-          }),
-        },
-      );
-      await HandleResponseError(response);
-      if (response.ok) {
-        console.log("User deleted successful " + firebaseId);
-      }
-      //reset store and timer here
-      logout();
+      deleteUser(firebaseUser);
+      ResetStorage();
     }
-  };
+  } catch (error) {
+    console.error("Error delete user:" + error);
+  }
 };
 
-export const login = (email, password) => {
-  return async (dispatch) => {
-    console.log("begin login");
+export const login = async (email, password) => {
+  console.log("begin login");
 
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        firebaseAuth,
-        email,
-        password,
-      );
+  try {
+    await signInWithEmailAndPassword(firebaseAuth, email, password);
 
-      const token = await getToken();
-      const localId = firebaseAuth.currentUser.uid;
-
-      console.log("logged in as", email);
-
-      await dispatch(authenticate(token, localId));
-
-      SaveCredentialsToStorage(email, password);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    SaveCredentialsToStorage(email, password);
+    console.log("logged in as", email);
+  } catch (error) {
+    console.error("error login:" + error);
+  }
 };
