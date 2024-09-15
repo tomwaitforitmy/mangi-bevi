27e65dfb29756b3e85294d1fcec3011944083f24
Thomas Kilgus <tomwaitforitmy@gmail.com>
2024-09-15 17:36:25 +0200

    Fix bug where split text would result in new lines for the user

--- components/HighlightedText.js
+++ components/HighlightedText.js
@@ -11,18 +11,23 @@ const HighlightedText = ({ text, searchTerm, highlightColor }) => {
   if (highlightText) {
     const textSplitted = SplitTextToHighlight(text, searchTerm);
 
-    return textSplitted.map((x, index) =>
-      x.toLowerCase() === searchTerm.toLowerCase() ? (
-        <Text
-          key={index}
-          style={{ color: highlightColor ? highlightColor : "red" }}>
-          {x}
-        </Text>
-      ) : (
-        <Text key={index} style={styles.regularText}>
-          {x}
-        </Text>
-      ),
+    return (
+      //extra text wrapper is needed, because else we get new lines for each text
+      <Text style={styles.regularText}>
+        {textSplitted.map((x, index) =>
+          x.toLowerCase() === searchTerm.toLowerCase() ? (
+            <Text
+              key={index}
+              style={{ color: highlightColor ? highlightColor : "red" }}>
+              {x}
+            </Text>
+          ) : (
+            <Text key={index} style={styles.regularText}>
+              {x}
+            </Text>
+          ),
+        )}
+      </Text>
     );
   }
 
