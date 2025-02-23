2be9324c5a92cc6e41b2bb6ca5190164bebbf3db
Thomas Kilgus <tomwaitforitmy@gmail.com>
2025-02-23 14:10:36 +0100

    Improve wording

--- data/AllowedSortingOptions.js
+++ data/AllowedSortingOptions.js
@@ -1,16 +1,16 @@
-export const LAST_CREATED = "Last created";
-export const FIRST_CREATED = "First created";
-export const LAST_EDITED = "Last edited";
+export const LAST_CREATED = "New";
+export const FIRST_CREATED = "Old";
+export const LAST_EDITED = "Recently edited";
 export const FIRST_EDITED = "First edited";
 export const MOST_COOKED = "Most cooked";
 export const MOST_REACTIONS = "Most reactions";
-export const CREATED_BY_ME = "Created by me";
+export const CREATED_BY_ME = "My own Mangis";
 
 export const ALLOWED_SORTING_OPTIONS = [
   LAST_CREATED,
   FIRST_CREATED,
   LAST_EDITED,
-  FIRST_EDITED,
+  // FIRST_EDITED, I think nobody would ever use this
   // Not implemented, yet:
   // MOST_COOKED,
   MOST_REACTIONS,
