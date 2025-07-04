2e24a914600d4316a484e3c9c3149f653d568e4d
Thomas Kilgus <tomwaitforitmy@gmail.com>
2025-07-04 13:08:37 +0200

    Fix issue where only the first test mangi would be deleted

--- firebase/deleteTestMangis.js
+++ firebase/deleteTestMangis.js
@@ -3,11 +3,11 @@ import * as mealsActions from "../store/actions/mealsAction";
 export async function deleteTestMangis(dispatch, allMeals, user) {
   const testMangis = allMeals.filter((m) => m.isTestMangi);
 
-  console.log(testMangis);
-
-  await Promise.all(
-    testMangis.map(async (m) => {
-      await dispatch(mealsActions.deleteMeal(m, user, allMeals));
-    }),
-  );
+  // Process deletions sequentially to avoid race conditions
+  let updatedUser = user;
+  for (const meal of testMangis) {
+    updatedUser = await dispatch(
+      mealsActions.deleteMeal(meal, updatedUser, allMeals),
+    );
+  }
 }
