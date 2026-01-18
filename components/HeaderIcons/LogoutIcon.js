612d3b9c7158b775a912a675b07d1123d8bb4d71
tomwaitforitmy <tomwaitforitmy@gmail.com>
2026-01-18 18:46:41 +0100

    Center save icon and logout icon

--- components/HeaderIcons/LogoutIcon.js
+++ components/HeaderIcons/LogoutIcon.js
@@ -2,7 +2,7 @@ import React from "react";
 import Colors from "../../constants/Colors";
 import * as authActions from "../../store/actions/authAction";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Alert } from "react-native";
+import { Alert, Pressable } from "react-native";
 import { ResetSecureStorage } from "../../common_functions/CredentialStorage";
 
 const onHeaderIconPress = () => {
@@ -23,12 +23,20 @@ const onHeaderIconPress = () => {
 
 const LogoutIcon = () => {
   return (
-    <Ionicons
-      name={"exit-outline"}
-      size={25}
-      color={Colors.navigationIcon}
+    <Pressable
       onPress={() => onHeaderIconPress()}
-    />
+      hitSlop={20}
+      android_ripple={null}
+      style={{
+        //somehow this size works nice for centering
+        width: 30,
+        height: 30,
+        paddingLeft: 8,
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+      <Ionicons name={"exit-outline"} size={25} color={Colors.navigationIcon} />
+    </Pressable>
   );
 };
 
--- components/HeaderIcons/SaveIcon.js
+++ components/HeaderIcons/SaveIcon.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Colors from "../../constants/Colors";
-import { Icon } from "react-native-elements";
-import IconTypes from "../../constants/IconTypes";
+import { Pressable } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const SaveIcon = (onPress) => {
   return (
-    <Icon
-      name={"save"}
-      onPress={onPress}
-      color={Colors.navigationIcon}
-      type={IconTypes.ionicon}
-    />
+    <Pressable
+      hitSlop={20}
+      style={{
+        //somehow this size works nice for centering
+        width: 36,
+        height: 36,
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+      <Ionicons
+        name="save"
+        size={25}
+        onPress={onPress}
+        color={Colors.navigationIcon}
+      />
+    </Pressable>
   );
 };
 
