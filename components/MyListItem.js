0fdc3968f45cf58d831abf74fca684faeaf1763d
tomwaitforitmy <tomwaitforitmy@gmail.com>
2026-02-01 15:06:48 +0100

    Replace icon

--- components/MyListItem.js
+++ components/MyListItem.js
@@ -1,16 +1,43 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import Hyperlink from "react-native-hyperlink";
-import { Icon } from "react-native-elements";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import Colors from "../constants/Colors";
 import HighlightedText from "./HighlightedText";
 import { PreTestSplit } from "../common_functions/SplitTextToHighlight";
+import IconTypes from "../constants/IconTypes";
 
 const MyListItem = (props) => {
   const testIconId = props.title + "-icon";
   const searchTerm = props.searchTerm;
 
   const highlightText = PreTestSplit(props.title, searchTerm);
+
+  const renderIcon = () => {
+    if (!props.IconName) {
+      return null;
+    }
+
+    const iconProps = {
+      name: props.IconName,
+      size: 24,
+      color: Colors.primary,
+      onPress: props.onPressIcon,
+      testID: testIconId,
+    };
+
+    // Determine which icon library to use based on iconType
+    if (props.iconType === IconTypes.ionicon) {
+      return <Ionicons {...iconProps} color={"black"} />;
+    }
+    // "edit" is from MaterialIcons
+    if (props.IconName === "edit") {
+      return <MaterialIcons {...iconProps} color={"black"} />;
+    }
+    return <MaterialCommunityIcons {...iconProps} color={"black"} />;
+  };
 
   return (
     <View style={styles.listItemView}>
@@ -28,14 +55,7 @@ const MyListItem = (props) => {
             </Hyperlink>
           )}
         </View>
-        {props.IconName && (
-          <Icon
-            name={props.IconName}
-            onPress={props.onPressIcon}
-            type={props.iconType}
-            testID={testIconId}
-          />
-        )}
+        {renderIcon()}
       </View>
     </View>
   );
