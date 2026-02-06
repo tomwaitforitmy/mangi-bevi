04d8426f1db9c1b39e9e41a6136e1d99374491fb
tomwaitforitmy <tomwaitforitmy@gmail.com>
2026-02-06 17:49:49 +0100

    Remove old expirements

--- experiments/MoveableElementContainer-Components/Element.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MyListItem from "../../components/MyListItem";
-
-function Element({ title }) {
-  return <MyListItem title={title} IconName={"edit"}></MyListItem>;
-}
-
-export default Element;
